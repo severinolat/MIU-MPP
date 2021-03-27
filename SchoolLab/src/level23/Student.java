3e6b9d6b95e433831b7d323a9c5501bd589f809a
Severin Lawson <severinlawson@Severins-MacBook-Pro.local>
2021-03-26 20:51:31 -0500

    first commit

---
+++ SchoolLab/src/level23/Student.java
@@ -0,0 +1,28 @@
+package level23;
+
+public class Student extends Person  {
+
+	private double gpa;
+	
+	public Student(String name, String phone, int age, double gpa) {
+		super(name, phone, age);
+		this.gpa = gpa;
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getGpa() {
+		return gpa;
+	}
+
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	
+	public double getSalary() {
+		return 0;
+	}
+
+	
+	
+	
+}
